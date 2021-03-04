9dd46715ef09fa4aff2c47671f84b2d325462f47
Enrico <enricosaccon96@gmail.com>
2021-03-04 08:40:02 +0100

    Dunno

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,23 @@
 #include<Arduino.h>
 
-int PINW=6;
-int PING=7;
+int PINWin=6, PINWout=4;
+int PINGin=7, PINGout=5;
 
 void setup() {
 	Serial.begin(9600);
-  // initialize digital pin PIN as an output.
-  pinMode(PINW, INPUT);
-  pinMode(PING, INPUT);
+  // initialize analog pin PIN as an output.
+  pinMode(PINWin, INPUT);
+  pinMode(PINGin, INPUT);
   Serial.println("Board ready");
 }
 
 // the loop function runs over and over again forever
 void loop() {
 	int vw=0, vg=0;
-  vw=digitalRead(PINW);
-  vg=digitalRead(PING);
+  vw=analogRead(PINWin);
+  vg=analogRead(PINGin);
   Serial.print("green: "); Serial.println(vg);
   Serial.print("white: "); Serial.println(vw);
+  analogWrite(PINWout, vw);
+  analogWrite(PINGout, vg);
 }
