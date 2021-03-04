37e991d0ee80880b082352a8208c0a431270031e
icosac <enricosaccon96@gmail.com>
2021-03-04 10:57:06 +0100

    LDE

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,39 @@
 #include<Arduino.h>
 
+enum STATUS {BLUET, CABLE};
+
+int PINLEDCI=8;
+int PINLEDCO=9;
+
 int PINW=6;
 int PING=7;
+
+int deviceStatus=STATUS::BLUET;
+
+void changeStatus(){
+  switch(deviceStatus){
+    case STATUS::BLUET:{
+      deviceStatus=STATUS::CABLE;
+      digitalWrite(PINLEDCO, HIGH);
+      break;
+    }
+    case STATUS::CABLE:{
+      deviceStatus=STATUS::BLUET;
+      digitalWrite(PINLEDCO, LOW);
+    }
+  }
+
+  delay(1000);
+}
 
 void setup() {
 	Serial.begin(9600);
   // initialize digital pin PIN as an output.
   pinMode(PINW, INPUT);
   pinMode(PING, INPUT);
+  pinMode(PINLEDCI, INPUT);
+  pinMode(PINLEDCO, OUTPUT);
+
   Serial.println("Board ready");
 }
 
@@ -18,4 +44,8 @@ void loop() {
   vg=digitalRead(PING);
   Serial.print("green: "); Serial.println(vg);
   Serial.print("white: "); Serial.println(vw);
+
+  if (digitalRead(PINLEDCI)==HIGH){
+    changeStatus();
+  }
 }
